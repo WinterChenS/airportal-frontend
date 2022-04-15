87c62a0d57d84a10e00f6efc378b5080da29d6e0
WinterChenS <1085143002@qq.com>
2022-04-15 10:22:13 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="hello">
+  <img class="logo" alt="Vue logo" src="../assets/logo.png">
+    <el-upload
+      class="upload-demo"
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-change="handleChange">
+      <!--<div @mouseenter="enter()" @mouseleave="leave()">
+        <el-button class="upload-button" size="small" type="primary">发送</el-button>
+      </div>-->
+      <el-popover
+        placement="top-start"
+        title="标题"
+        width="200"
+        offset=-180
+        visible-arrow=false
+        trigger="hover"
+        content="这是一段内容,这是一段内容,这是一段内容,这是一段内容。">
+        <el-button class="upload-button" size="small" type="primary" slot="reference">发送</el-button>
+      </el-popover>
+    </el-upload>
+    <el-row>
+      <el-button class="receive-button" plain>接收</el-button>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  export default {
+    // data() {
+    //   return {
+    //     fileList: [{
+    //       name: 'food.jpeg',
+    //       url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+    //     }, {
+    //       name: 'food2.jpeg',
+    //       url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+    //     }]
+    //   };
+    // },
+    methods: {
+      handleChange(file, fileList) {
+        this.fileList = fileList.slice(-3);
+      },
+      enter() {
+        
+      },
+      leave() {
+        
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello {
+  top: 100px;
+  .upload-button {
+    width: 224px;
+    height: 49px;
+    border-radius: 10px;
+    font-size: 15px;
+  }
+  .receive-button {
+    margin-top: 15px;
+    width: 224px;
+    height: 49px;
+    border-radius: 10px;
+    font-size: 15px;
+  }
+  .logo {
+    margin-bottom: 100px;
+  }
+  
+}
+
+
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,92 @@
+<template>
+      <div class="app-box">
+        <div class="menu-nav">
+          <el-dropdown>
+            <el-button class="menu-button"  plain>
+            <i class="el-icon-s-operation"></i>
+            </el-button>
+            <el-dropdown-menu class="menu-item" slot="dropdown">
+              <el-dropdown-item>黄金糕</el-dropdown-item>
+              <el-dropdown-item>狮子头</el-dropdown-item>
+              <el-dropdown-item>螺蛳粉</el-dropdown-item>
+              <el-dropdown-item>双皮奶</el-dropdown-item>
+              <el-dropdown-item>蚵仔煎</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </div>
+        <div class="home">
+          <HelloWorld msg="Welcome to Your Vue.js App"/>
+        </div>
+      </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+  
+  .app-box {
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 80px);
+    .menu-nav {
+      height: 100px;
+      text-align: right;
+    }
+    .home {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      flex-grow: 1;
+    }
+    .el-dropdown {
+      vertical-align: top;
+    }
+    .el-dropdown + .el-dropdown {
+      margin-left: 15px;
+    }
+    .el-icon-arrow-down {
+      font-size: 12px;
+    }
+    .menu-button {
+      border: 0px solid #DCDFE6;
+    }
+    .el-icon-s-operation {
+      font-size: 30px;
+    }
+    
+  }
+  .menu-item {
+      transform-origin: center top;
+      display: block;
+      z-index: 2017;
+      width: 200px;
+      border-radius: 2px;
+      top: -10px;
+      ::v-deep .popper__arrow {
+        display: none;
+      }
+    }
+  
+  
+</style>
+<style lang="css">
+.menu-item {
+      transform-origin: center top;
+      z-index: 2017;
+      width: 200px;
+      border-radius: 2px;
+      top: -10px !important;
+    }
+</style>
